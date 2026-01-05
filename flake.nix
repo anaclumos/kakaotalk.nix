847db6f2b84c4401602aa8a55f2c60e27397e7cd
Sunghyun Cho <hey@cho.sh>
2026-01-05 12:30:27 +0900

    fix: update flake.nix description and improve font handling for KakaoTalk

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
-  description = "A Nix flake for KakaoTalk";
+  description = "KakaoTalk for NixOS via Wine";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       inherit (nixpkgs.lib) concatMapStringsSep genAttrs;
-
       systems = [ "x86_64-linux" ];
 
       westernFonts = [
@@ -31,54 +31,60 @@
         "Corbel"
       ];
 
-      koreanFonts = [ "Gulim" "Dotum" "Batang" "Gungsuh" "Malgun Gothic" ];
+      koreanFonts = [
+        "Gulim"
+        "Dotum"
+        "Batang"
+        "Gungsuh"
+        "Malgun Gothic"
+      ];
+      quoteList = l: concatMapStringsSep " " (f: ''"${f}"'') l;
 
-      quoteList = list:
-        concatMapStringsSep " " (font: ''"'' + font + ''"'') list;
-
-      mkPackage = system:
+      mkPackage =
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          sources = pkgs.callPackage ./_sources/generated.nix {};
-
-          fontPackages = with pkgs; [ noto-fonts-color-emoji pretendard ];
+          sources = pkgs.callPackage ./_sources/generated.nix { };
           fontPath = pkgs.symlinkJoin {
             name = "kakaotalk-fonts";
-            paths = fontPackages;
+            paths = with pkgs; [
+              noto-fonts-color-emoji
+              pretendard
+            ];
           };
-
           desktopItem = pkgs.makeDesktopItem {
             name = "kakaotalk";
             exec = "kakaotalk %U";
             icon = "kakaotalk";
             desktopName = "KakaoTalk";
             genericName = "Instant Messenger";
-            comment = "A messaging and video calling app";
-            categories = [ "Network" "InstantMessaging" ];
+            comment = "Messaging and video calling";
+            categories = [
+              "Network"
+              "InstantMessaging"
+            ];
             mimeTypes = [ "x-scheme-handler/kakaotalk" ];
             startupWMClass = "kakaotalk.exe";
           };
-        in pkgs.stdenv.mkDerivation rec {
+        in
+        pkgs.stdenv.mkDerivation {
           pname = "kakaotalk";
           version = "0.2.0";
           src = sources.kakaotalk-exe.src;
           dontUnpack = true;
-
-          nativeBuildInputs = [ pkgs.wineWowPackages.stable pkgs.winetricks ];
-
-          # Optional runtime dependencies for enhanced reliability features
-          # - xdotool: window activation, phantom window hiding, watchdog
-          # - wmctrl: alternative window activation method
-          propagatedBuildInputs = [ pkgs.xdotool pkgs.wmctrl ];
-
+          nativeBuildInputs = [
+            pkgs.wineWowPackages.stable
+            pkgs.winetricks
+          ];
+          propagatedBuildInputs = [
+            pkgs.xdotool
+            pkgs.wmctrl
+          ];
           installPhase = ''
             runHook preInstall
-
-            install -Dm644 ${sources.kakaotalk-icon.src} \
-              $out/share/icons/hicolor/scalable/apps/kakaotalk.svg
-            install -Dm644 ${src} $out/share/kakaotalk/KakaoTalk_Setup.exe
+            install -Dm644 ${sources.kakaotalk-icon.src} $out/share/icons/hicolor/scalable/apps/kakaotalk.svg
+            install -Dm644 $src $out/share/kakaotalk/KakaoTalk_Setup.exe
             install -Dm755 ${./wrapper.sh} $out/bin/kakaotalk
-
             substituteInPlace $out/bin/kakaotalk \
               --replace-fail "@bash@" "${pkgs.bash}" \
               --replace-fail "@wineBin@" "${pkgs.wineWowPackages.stable}/bin" \
@@ -88,34 +94,37 @@
               --replace-fail "@westernFonts@" '${quoteList westernFonts}' \
               --replace-fail "@koreanFonts@" '${quoteList koreanFonts}' \
               --replace-fail "@fontPath@" "${fontPath}/share/fonts"
-
-            install -Dm644 ${desktopItem}/share/applications/kakaotalk.desktop \
-              $out/share/applications/kakaotalk.desktop
-
+            install -Dm644 ${desktopItem}/share/applications/kakaotalk.desktop $out/share/applications/kakaotalk.desktop
             runHook postInstall
           '';
-
           meta = with pkgs.lib; {
-            description = "A messaging and video calling app.";
-            homepage =
-              "https://www.kakaocorp.com/page/service/service/KakaoTalk";
+            description = "KakaoTalk messenger";
+            homepage = "https://www.kakaocorp.com/page/service/service/KakaoTalk";
             license = licenses.unfree;
             platforms = [ "x86_64-linux" ];
           };
         };
-    in {
-      packages = genAttrs systems (system: rec {
-        kakaotalk = mkPackage system;
-        default = kakaotalk;
+    in
+    {
+      packages = genAttrs systems (s: {
+        kakaotalk = mkPackage s;
+        default = mkPackage s;
       });
-
-      apps = genAttrs systems (system:
-        let pkg = self.packages.${system}.kakaotalk; in rec {
+      apps = genAttrs systems (
+        s:
+        let
+          p = mkPackage s;
+        in
+        {
           kakaotalk = {
             type = "app";
-            program = "${pkg}/bin/kakaotalk";
+            program = "${p}/bin/kakaotalk";
           };
-          default = kakaotalk;
-        });
+          default = {
+            type = "app";
+            program = "${p}/bin/kakaotalk";
+          };
+        }
+      );
     };
 }
